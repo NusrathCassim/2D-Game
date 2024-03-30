0b90c0663dfc6c280d7a6d18eac8e6c2730e1556
Nusrath cassim <nusrathcassim1@gmail.com>
2024-03-30 18:07:46 +0530

    little-update

--- ChickGame/src/object/Object_heart.java
+++ ChickGame/src/object/Object_heart.java
@@ -2,15 +2,15 @@ package object;
 import Character.Character;
 import main.GamePanel;
 
-public class Heart extends Character{
-	
-	public Heart(GamePanel gp) {
+public class Object_heart extends Character {
+
+	public Object_heart(GamePanel gp) {
 		super(gp);
+		// TODO Auto-generated constructor stub
 		name = "Heart";
 		image = setup("/objects/fullH");
 		image2 = setup("/objects/HalfH");
 		image3 = setup("/objects/EmptyH");
-	
-		
 	}
+
 }
